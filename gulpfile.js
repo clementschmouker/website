83c6927d5f5c0d62e745053a89653d7ceb064e10
clement <clement.schmouker@gmail.com>
2017-11-07 09:40:35 +0100

    working now, ready for tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,42 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var browserSync = require('browser-sync').create();
+var babel = require('gulp-babel');
+var webpack = require('webpack');
+var webpackStream = require('webpack-stream');
+var webpackConfig = require('./webpack.config.js');
 
-
+// Tasks
 gulp.task('sass', function() {
 	return gulp.src('src/assets/scss/main.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('dist/assets/_styles'))
+	.pipe(browserSync.reload({
+		stream: true,
+	}));
+});
+
+gulp.task('js', function() {
+	return gulp.src('src/assets/js/main.js')
+	.pipe(webpackStream(webpackConfig), webpack)
+	.pipe(babel())
+	.pipe(gulp.dest('dist/assets/_js'));
 })
+
+gulp.task('watch', ['browserSync', 'sass', 'js'], function() {
+	gulp.watch('src/assets/scss/**/*.scss', ['sass']);
+	gulp.watch('*.html', browserSync.reload);
+	gulp.watch('src/assets/js/**/*.js', ['js']);
+	gulp.watch('src/assets/js/**/*.js', browserSync.reload);
+});
+
+
+// Browser Sync
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		},
+	});
+});
