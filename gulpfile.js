7591ac4a138ce8e8b67a3d3b79b58f4e74749a98
clement <clement.schmouker@gmail.com>
2017-11-08 14:44:09 +0100

    added uglify to the watch task

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,10 @@ var babel = require('gulp-babel');
 var webpack = require('webpack');
 var webpackStream = require('webpack-stream');
 var webpackConfig = require('./webpack.config.js');
+var uglify = require('gulp-uglify');
 
 // Tasks
-gulp.task('sass', function() {
+gulp.task('sass', () => {
 	return gulp.src('src/assets/scss/main.scss')
 	.pipe(sass())
 	.pipe(gulp.dest('dist/assets/_styles'))
@@ -16,19 +17,20 @@ gulp.task('sass', function() {
 	}));
 });
 
-gulp.task('js', function() {
+gulp.task('js', () => {
 	return gulp.src('src/assets/js/main.js')
 	.pipe(webpackStream(webpackConfig), webpack)
 	.pipe(babel())
+	.pipe(uglify())
 	.pipe(gulp.dest('dist/assets/_js'));
 })
 
-gulp.task('img', function() {
+gulp.task('img', () => {
 	return gulp.src(['src/assets/img/**/*.jpg', 'src/assets/img/**/*.jpeg', 'src/assets/img/**/*.png', 'src/assets/img/**/*.gif'])
 	.pipe(gulp.dest('dist/assets/_img'));
 })
 
-gulp.task('watch', ['browserSync', 'img', 'sass', 'js'], function() {
+gulp.task('watch', ['browserSync', 'img', 'sass', 'js'], () => {
 	gulp.watch('src/assets/img/**/*{jpg,jpeg,png,gif}', ['img']);
 	gulp.watch('src/assets/scss/**/*.scss', ['sass']);
 	gulp.watch('*.html', browserSync.reload);
@@ -38,7 +40,7 @@ gulp.task('watch', ['browserSync', 'img', 'sass', 'js'], function() {
 
 
 // Browser Sync
-gulp.task('browserSync', function() {
+gulp.task('browserSync', () => {
 	browserSync.init({
 		server: {
 			baseDir: './'
